41102cf9a8e5e8e3b3803b8d1445ef37ce92a044
a01048197443 <a01048197443@gmail.com>
2022-03-08 16:51:46 +0900

    11. 글쓰기 완료

--- dbproject/src/main/java/site/metacoding/dbproject/web/PostController.java
+++ dbproject/src/main/java/site/metacoding/dbproject/web/PostController.java
@@ -1,17 +1,33 @@
 package site.metacoding.dbproject.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.dbproject.domain.post.Post;
+import site.metacoding.dbproject.domain.post.PostRepository;
+import site.metacoding.dbproject.domain.user.User;
+
+@RequiredArgsConstructor // final이 붙은 애들에 대한 생성자를 만들어준다.
 @Controller
 public class PostController {
+
+    private final HttpSession session;
+    private final PostRepository postRepository;
+
     // GET 글쓰기 페이지 /post/writeForm - 인증 O
     @GetMapping("/s/post/writeForm")
     public String writeForm() {
+        if (session.getAttribute("principal") == null) {
+            return "redirect:/loginForm";
+        }
         return "post/writeForm";
     }
 
@@ -19,7 +35,11 @@ public class PostController {
     // GET 글목록 페이지 /post/list, /
     // @GetMapping({"/", "/post/list"})
     @GetMapping({ "/", "/post/list" })
-    public String list() {
+    public String list(Model model) {
+        // 1. postRepository의 findAll() 호출
+
+        // 2. model에 담기
+
         return "post/list";
     }
 
@@ -49,7 +69,15 @@ public class PostController {
 
     // POST 글쓰기 /post - 글목록으로 가기 - 인증 O
     @PostMapping("/s/post")
-    public String write() {
+    public String write(Post post) {
+        // title, content 1. null 검사, 2. 공백검사, 3. 길이검사 ____
+        if (session.getAttribute("principal") == null) {
+            return "redirect:/loginForm";
+        }
+        User principal = (User) session.getAttribute("principal");
+        post.setUser(principal);
+        // insert into post(title, content, userId) values(사용자, 사용자, 세션오브젝트의 PK)
+        postRepository.save(post);
         return "redirect:/";
     }
 }
