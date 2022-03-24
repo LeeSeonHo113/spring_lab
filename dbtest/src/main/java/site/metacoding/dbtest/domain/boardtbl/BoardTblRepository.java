2775ad397ec2bdb708e0ac4fa242e5baca2c6bc8
a01048197443 <a01048197443@gmail.com>
2022-03-24 09:56:55 +0900

    5. home 페이지에 boardTbl 전체목록 출력하기

---
+++ dbtest/src/main/java/site/metacoding/dbtest/domain/boardtbl/BoardTblRepository.java
@@ -0,0 +1,7 @@
+package site.metacoding.dbtest.domain.boardtbl;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardTblRepository extends JpaRepository<BoardTbl, Integer> {
+
+}
